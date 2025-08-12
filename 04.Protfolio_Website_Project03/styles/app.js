eb9296d8808700991bdd6e2cde169e514a569e5c
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-12 10:14:29 +0900

    rwd設定完成

--- 04.Protfolio_Website_Project03/styles/app.js
+++ 04.Protfolio_Website_Project03/styles/app.js
@@ -1,9 +1,10 @@
 let nav = document.querySelector("nav");
 
 window.addEventListener("scroll", () => {
-  if (window.screenY == 0) {
+  if (window.scrollY == 0) {
     nav.style.boxShadow = "";
   } else {
     nav.style.boxShadow = "0 10px 6px -6px #777";
   }
 });
+//滑動的時候下面會出現陰影
