b3ab7a3ef3430a5bbc42ecf92ac8bb30541f2c6d
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-13 16:00:45 +0900

    完成7-5

--- 07.Document_Object_Model/05.Arrow_Function_Expression/app.js
+++ 07.Document_Object_Model/05.Arrow_Function_Expression/app.js
@@ -1,1 +1,16 @@
-init;
+//方法1
+let hello = () => {
+  console.log("hw");
+};
+
+hello();
+
+//方法2
+let Wilson = {
+  name: "Willson",
+  walk: () => {
+    console.log("Wilson is walking..");
+  },
+};
+
+Wilson.walk();
