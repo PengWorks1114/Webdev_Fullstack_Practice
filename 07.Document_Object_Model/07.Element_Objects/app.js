e6ba16eec61c7a3d819f31df7d29a7de262e70e2
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-13 17:40:24 +0900

    更新中

--- 07.Document_Object_Model/07.Element_Objects/app.js
+++ 07.Document_Object_Model/07.Element_Objects/app.js
@@ -1,1 +1,24 @@
-init;
+//addEventListener(event, callbackFn)
+let myButton = document.querySelector("#my-btn");
+
+myButton.addEventListener("click", () => {
+  alert("你點了botton");
+});
+
+// appendChild(element)
+let body = document.querySelector("body");
+
+// document object
+let myH1 = document.createElement("h1");
+// innerHTML, innerText
+myH1.innerText = "這是我的h1";
+// myH1.innerHTML = "<a>這是我的h1</a>";
+body.appendChild(myH1);
+
+console.log(body.children); //第一層
+console.log(body.children[0].children[0]); //下一層
+
+//parentElement
+let firstP = document.querySelector("p");
+
+console.log(firstP.parentElement.parentElement);
