e39ad409008b67519c0d8518c983495c0981a877
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-13 10:26:09 +0900

    08 init

---
+++ 06.JavaScript_Basics02/08.Number_Guessing_Game/app.js
@@ -0,0 +1,19 @@
+for (let i = 0; i < 11; i++) {
+  console.log(i);
+}
+
+console.log("forloop執行完畢");
+
+// while ("true") {
+//   console.log("這個程式碼正在被執行...")
+// };
+//無限迴圈
+
+// 正確用法
+// 宣告一個計數器
+let i = 0;
+
+while (i < 10) {
+  console.log(i);
+  i++; //這一步最重要,少了這裡他還是永遠會是0,就會變成無限迴圈
+}
