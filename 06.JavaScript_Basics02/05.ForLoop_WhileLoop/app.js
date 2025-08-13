6592313fb52d2b374e3c9fcd31ed00a5a0a8490f
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-13 09:31:03 +0900

    完成05 forwhileloop

--- 06.JavaScript_Basics02/05.ForLoop_WhileLoop/app.js
+++ 06.JavaScript_Basics02/05.ForLoop_WhileLoop/app.js
@@ -1,40 +1,19 @@
-let Wilson = {
-  //properties (key-value pair), methods
-  first_name: "Wilson",
-  last_name: "Ren",
-  age: 26,
-  is_married: true,
-  spouse: "Grace",
-
-  sayHi() {
-    console.log("Wilson says hi");
-  },
-  walk() {
-    console.log("Wilson is walking");
-  },
-  speak(words) {
-    console.log("Wilson says " + words);
-  },
-  sayThis() {
-    console.log(this);
-  },
-};
-
-console.log(Wilson.first_name); //獲取物件 方法一
-console.log(Wilson[first_name]); //方法二
-
-Wilson.sayHi();
-Wilson.speak("今天心情不錯");
-
-Wilson.sayThis(this.first_name + "say hi"); //指的是Wilson這個物件
-
-let arr = [1, 2, 3, 4, 5];
-console.log(typeof arr); //不行直接檢查 會顯示object
-
-function hello() {
-  console.log("hello world");
+for (let i = 0; i < 11; i++) {
+  console.log(i);
 }
 
-console.log(typeof hello); //function
+console.log("forloop執行完畢");
 
-//function, array are both special types of object
+// while ("true") {
+//   console.log("這個程式碼正在被執行...")
+// };
+//無限迴圈
+
+// 正確用法
+// 宣告一個計數器
+let i = 0;
+
+while (i < 10) {
+  console.log(i);
+  i++; //這一步最重要,少了這裡他還是永遠會是0,就會變成無限迴圈
+}
