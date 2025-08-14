9f5566e0b79b23c1e0cfeac9c0657c0bf086d2da
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-14 16:24:31 +0900

    進場動畫完成

--- 08.Grade-Calculator_Project04/app.js
+++ 08.Grade-Calculator_Project04/app.js
@@ -1,3 +1,19 @@
 let hero = document.querySelector(".hero");
 let slider = document.querySelector(".slider");
 let animation = document.querySelector("section.animation-wrapper");
+
+const time_line = new TimelineMax();
+
+//parameter1 是要控制的對象
+//parameter2 是duration
+//parameter3 是控制對象的原始狀態
+//parameter4 是控制對象的動畫結束的狀態
+time_line
+  .fromTo(hero, 1, { height: "0%" }, { height: "100%", ease: Power2.easeInOut })
+  .fromTo(
+    hero,
+    1.2,
+    { width: "80%" },
+    { width: "100%", ease: Power2.easeInOut }
+  )
+  .fromTo(slider, 1, { x: "-100%" }, { x: "0%", ease: Power2.easeInOut });
