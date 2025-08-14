d1fa58c086d0741af93270c7c15865f493911af1
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-14 16:31:43 +0900

    animation搞定

--- 08.Grade-Calculator_Project04/app.js
+++ 08.Grade-Calculator_Project04/app.js
@@ -8,6 +8,7 @@ const time_line = new TimelineMax();
 //parameter2 是duration
 //parameter3 是控制對象的原始狀態
 //parameter4 是控制對象的動畫結束的狀態
+//parameter5 提早幾秒
 time_line
   .fromTo(hero, 1, { height: "0%" }, { height: "100%", ease: Power2.easeInOut })
   .fromTo(
@@ -16,4 +17,11 @@ time_line
     { width: "80%" },
     { width: "100%", ease: Power2.easeInOut }
   )
-  .fromTo(slider, 1, { x: "-100%" }, { x: "0%", ease: Power2.easeInOut });
+  .fromTo(
+    slider,
+    1,
+    { x: "-100%" },
+    { x: "0%", ease: Power2.easeInOut },
+    "-=1.2"
+  )
+  .fromTo(animation, 0.3, { opacity: 1 }, { opacity: 0 }); //從1到0代表整個畫面消失在眼前顯示我們的網站
