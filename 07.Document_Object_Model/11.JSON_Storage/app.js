fecadc0e1274e497a4a6c67f63b501bb3f0fae28
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-14 10:35:46 +0900

    7-11

---
+++ 07.Document_Object_Model/11.JSON_Storage/app.js
@@ -0,0 +1,5 @@
+window.localStorage.setItem("name", "Wilson"); // window可以省略
+window.localStorage.setItem("age", "26"); // 26在這邊被存入是String
+//這兩個localStorage都可以在f12的本機儲存空間裡面找到
+
+console.log(localStorage.getItem("nnaammee")); // null 不存在的東西
