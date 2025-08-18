f68519295692869c961a4774771341f870be6990
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-18 17:23:05 +0900

    10 init

--- 09.Advanced_JavaScript01/02.Spread_Syntax_and_Rest_Parameters/app.js
+++ 09.Advanced_JavaScript01/02.Spread_Syntax_and_Rest_Parameters/app.js
@@ -5,6 +5,29 @@ let num2 = [4, 5, 6];
 let result = num1 + num2;
 console.log(typeof result); //String 這不是我們想要的結果
 
-// 做法1
+// 做法
 let result1 = num1.concat(num2);
 console.log(result1);
+
+
+const parts = ["肩膀", "膝蓋"];
+const otherParts = ["頭", ...parts, "身體", "腳"];
+console.log(otherParts); //合在一起
+
+// 複製array
+// 不是 copy by reference
+const arr = [1, 2, 3];
+const arr2 = [...arr];
+
+arr2.push(4);
+
+console.log(arr); //123
+console.log(arr2); //1234
+
+//array concat
+
+let arr3 = [1, 2, 3];
+let arr4 = [4, 5, 6];
+
+console.log([...arr, ...arr2]);
+
