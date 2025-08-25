2c90f6324dc4c3c2415c7436fb148ea755395549
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-25 13:57:45 +0900

    13 init

--- 10.Advanced_JacaScript02/09.Class/app.js
+++ 10.Advanced_JacaScript02/09.Class/app.js
@@ -1,34 +1,4 @@
-// // Function Person()
-// function Person(name, age) {
-//   this.name = name;
-//   this.age = age;
-// }
-
-// Person.prototype.sayHi = function () {
-//   console.log(this.name, "說你好");
-// };
-
-// // Function Student()
-// function Student(name, age, major, grade) {
-//   Person.call(this, name, age);
-//   this.major = major;
-//   this.grade = grade;
-// }
-
-// Student.prototype = Object.create(Person.prototype); // 要跟下面注意順序 不然會複寫!
-
-// Student.prototype.study = function () {
-//   console.log(this.name + "正就讀" + this.major);
-// }; // 要跟上面注意順序 不然會複寫!
-
-// // 設定 A.prototype = Object.created(B.prototype)。
-// // Object.create() 可以創建一個全新的物件。
-// // 這樣一來, 所有在 B.prototype 內部的 methods 都可以套用給 A.prototype。
-// // 所有 A.prototype 所設定的額外 methods 都需要寫在 A.prototype = Object.create(B.prototype) 這行程式碼的下方。
-
-// let mike = new Student("Mike", 26, "Chemistry", 3.5);
-// mike.sayHi(); // 可執行 因為Student.prototype = Object.create(Person.prototype);
-// mike.study(); // 可執行
+//Class改寫 練習
 
 function Student(name, age, major) {
   this.name = name;
--- 10.Advanced_JacaScript02/09.Class/app3.js
+++ 10.Advanced_JacaScript02/09.Class/app3.js
@@ -18,17 +18,24 @@ Student.prototype.sayHi = function () {
 // Class寫法改寫
 
 class Student {
-  static exampleProperty = 10;
+  static exampleProperty = 10; // static property
 
   constructor(name, age, major) {
-    this.name = name;
-    this.age = age;
-    this.major = major;
+    this.name = name; // instance property
+    this.age = age; // instance property
+    this.major = major; // instance property
   }
+
+  // instance method
   sayHi() {
     console.log(this.name + "說你好");
   }
+
+  // static method
   static exampleFunction() {
     console.log("這是一個沒有特別功能的function");
   }
 }
+
+let mike = new Student("Mike", 25, "chemistry");
+Student.exampleFunction();
