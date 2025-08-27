a2c2e15d98d166d47d2e9574e7dde11b88dff982
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-27 09:27:00 +0900

    update

--- 15.Node.js/03.Node內建Module/app.js
+++ 15.Node.js/03.Node內建Module/app.js
@@ -1,2 +1,13 @@
 // fs (file system)
 const fs = require("fs");
+
+fs.writeFile("myFile.txt", "今天天氣不錯", (e) => {
+  if (e) throw e;
+
+  console.log("文件已經撰寫完畢");
+});
+
+fs.readFile("myFile.txt", "utf8", (e, data) => {
+  if (e) throw e;
+  console.log(data);
+});
